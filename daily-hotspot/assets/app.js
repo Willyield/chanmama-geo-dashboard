cf10d31152aa72f85f2984ef0a525394b1db8305
Willyield <idtoure93@gmail.com>
2026-08-06 16:56:16 +0800

    Publish complete creator audit 2026-08-06

--- daily-hotspot/assets/app.js
+++ daily-hotspot/assets/app.js
@@ -15,7 +15,7 @@ import {
   creatorViews,
   renderCreatorDetail,
   renderCreatorPage,
-} from "./creators.js?v=20260806complete1216r1";
+} from "./creators.js?v=20260806afternoon1600r1";
 import { applyCreatorIdentityCorrection } from "./creator-data.js";
 import { escapeHtml, icon, renderError } from "./ui.js";
 
